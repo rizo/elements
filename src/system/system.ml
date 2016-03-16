6578b09ee1a7adb960147e9ce2ebdc66e986b4e3
Rizo Isrof <rizo@movvo.com>
2016-03-16 01:02:55 +0000

    Fixed fmt string in Data.System.time

--- src/system/system.ml
+++ src/system/system.ml
@@ -6,7 +6,7 @@ let time ?fmt f x =
   let fx = f x in
   let t1 = Unix.gettimeofday () -. t0 in
   let () = match fmt with
-  | Some fmt -> Printf.eprintf "%s" (fmt t1)
+  | Some fmt -> Printf.eprintf "%s\n" (fmt t1)
   | None     -> Printf.eprintf "Elapsed time: %f sec\n" t1 in
   fx
 
