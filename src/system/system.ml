a7ac9e9840f574b162aa68d5ad87041d8f77bb74
Rizo Isrof <rizo@movvo.com>
2016-03-16 00:59:40 +0000

    Fixed type bug in Data.System.time

--- src/system/system.ml
+++ src/system/system.ml
@@ -5,8 +5,8 @@ let time ?fmt f x =
   let t0 = Unix.gettimeofday () in
   let fx = f x in
   let t1 = Unix.gettimeofday () -. t0 in
-  match fmt with
+  let () = match fmt with
   | Some fmt -> Printf.eprintf "%s" (fmt t1)
-  | None     -> Printf.eprintf "Elapsed time: %f sec\n" t1;
+  | None     -> Printf.eprintf "Elapsed time: %f sec\n" t1 in
   fx
 
