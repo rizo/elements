c75a9eadc87ce35620ba3144a6ee9d5fe429c091
Rizo Isrof <rizo@movvo.com>
2016-06-22 16:35:10 +0100

    Added Chan to public API.

--- src/elements.ml
+++ src/elements.ml
@@ -1,9 +1,10 @@
 
-let version = "0.13.0"
+let version = "0.14.0"
 
 module Control = Control
 module System  = System
 module IO      = System.IO
+module Chan    = System.IO.Chan
 
 include Base
 include Data
