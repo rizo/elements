ac1f1640a7acfabb71a75ac8d275aabe32a0976b
Rizo Isrof <rizo@odis.io>
2017-09-01 20:05:17 +0100

    Remove pkg files

--- pkg/pkg.ml
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ocaml
-#use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "proto" @@ fun c ->
-  Ok [ Pkg.mllib "src/proto.mllib";
-       Pkg.test "test/test"; ]
